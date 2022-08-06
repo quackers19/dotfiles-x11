d7523f327e355d81dba50bdf5872ff225c8adafe
squirty19 <liam.j.tighe@gmail.com>
2022-08-06 13:12:46 +1000

    started setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
 #/bin/bash
+
+print_message "Updating repositories:"
+sudo pacman -Syu
+print_message "Finished updating repositories!"
+
+print_message "Installing base-devel:"
+sudo pacman -Syy --needed --noconfirm base-devel
+print_message "base-devel installed!"
+
+print_message "Installing yay:"
+cd /tmp && git clone https://aur.archlinux.org/yay && cd /tmp/yay && makepkg -si
+print_message "Yay installed!"
