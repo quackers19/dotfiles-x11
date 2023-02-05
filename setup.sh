700870f8c5377fc493f08dd23b7c45569f30eeb6
quackers19 <77521344+quackers19@users.noreply.github.com>
2023-02-05 13:42:55 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -81,7 +81,6 @@ sh install-oh-my-zsh.sh --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 echo "Oh-my-zsh installed!"
 
 echo "adding configs"
